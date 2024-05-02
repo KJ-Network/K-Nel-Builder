2ee61549acc1277ca0a4248afadd14b5bda546c0
KNKJ55 <knkj55@proton.me>
2024-05-03 05:48:00 +0800

    upload

---
+++ Sources/upload-sources.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+echo 'Please select a device:'
+echo '1. Meizu M6 Note (M1721)'
+echo '2. Exit'
+echo 'Enter numbers to select your device:'
+read -r repo
+if [ $repo = 1 ]
+then
+    cd ./Sources/M1721
+    if [ -s out ]
+        then rm -rf out
+    fi
+    git submodule add https://github.com/tiann/kernelsu.git KernelSU
+    git add .
+    git commit -m "Automatically update source code using scripts"
+    git push origin Kernel
+fi
+if [ $repo = 2 ]
+then exit
+fi

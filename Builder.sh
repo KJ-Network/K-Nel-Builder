685557b04da95aa3536ec2de7ac7b7b593cb50f0
KNKJ55 <knkj55@proton.me>
2024-05-04 19:17:47 +0800

    upload-builder

--- Builder.sh
+++ Builder.sh
@@ -13,27 +13,34 @@ if [ $option = 1 ]
     then
     echo 'Please select a device:'
     echo '1. Meizu M6 Note (M1721)'
-    echo '2. Exit'
+    echo '2. Redmi Note 4X (Mido)'
+    echo '3. Exit'
     echo 'Enter numbers to select your device:'
     read -r device
     if [ $device = 1 ]
         then 
         cd ./Sources/M1721/
         bash build-kernel.sh | tee /root/K-Nel-Projects/OutFiles/Build-Kernel-M1721.log
-        if [ -e '/root/K-Nel-Projects/OutFiles/K-Nel-M1721.zip' ]
-          then rm '/root/K-Nel-Projects/OutFiles/K-Nel-M1721.zip'
+        echo 'Build Successed!'
+    fi
+    if [ $device = 2 ]
+        then 
+        cd ./Sources/Mido/
+        bash build-kernel.sh | tee /root/K-Nel-Projects/OutFiles/Build-Kernel-Mido.log
+        if [ -e '/root/K-Nel-Projects/OutFiles/K-Nel-Mido.zip' ]
+          then rm '/root/K-Nel-Projects/OutFiles/K-Nel-Mido.zip'
         fi
         if [ -e out/arch/arm64/boot/Image.gz ]
         then
             echo 'Pack Starting······'
             cp -r out/arch/arm64/boot/Image.gz /root/K-Nel-Projects/Toolchain/Anykernel3/
             cd /root/K-Nel-Projects/Toolchain/Anykernel3/
-            zip -r '/root/K-Nel-Projects/OutFiles/K-Nel-M1721.zip' *
+            zip -r '/root/K-Nel-Projects/OutFiles/K-Nel-Mido.zip' *
         else
         echo 'Build Failed!'
         exit
         fi
-        if [ -s '/root/K-Nel-Projects/OutFiles/K-Nel-M1721.zip' ]
+        if [ -s '/root/K-Nel-Projects/OutFiles/K-Nel-Mido.zip' ]
           then echo 'Pack Successed!'
           else
             echo 'Pack Failed!'
@@ -41,7 +48,7 @@ if [ $option = 1 ]
         fi
         echo 'Build Successed!'
     fi
-    if [ $device = 2 ]
+    if [ $device = 3 ]
         then exit
     fi
 fi
--- Sources/upload-sources.sh
+++ Sources/upload-sources.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 echo 'Please select a device:'
 echo '1. Meizu M6 Note (M1721)'
-echo '2. Exit'
+echo '2. Redmi Note 4X (Mido)'
+echo '3. Exit'
 echo 'Enter numbers to select your device:'
 read -r repo
 if [ $repo = 1 ]
@@ -13,5 +14,13 @@ then
     git push origin Kernel
 fi
 if [ $repo = 2 ]
+then
+    cd ./Sources/Mido
+    git submodule add https://github.com/tiann/kernelsu.git KernelSU
+    git add .
+    git commit -m "Automatically update source code using scripts"
+    git push origin Kernel
+fi
+if [ $repo = 3 ]
 then exit
 fi
